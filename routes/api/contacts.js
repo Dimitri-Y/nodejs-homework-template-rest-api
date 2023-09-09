f7b7e16f3f3405a2ee40e4c06c6dcc5bca31ebb0
Dimitri-Y <dimoon.bond@gmail.com>
2023-09-09 14:01:02 +0300

    first iteration

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,55 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 const { validateBody } = require("../../middlewares/validation");
-const contacts = require("../../models/contacts");
-const { nanoid } = require("nanoid");
-const schemas = require("../../schema");
+const schemas = require("../../service/schemas/schema");
+const ctrlContact = require("../../controllers");
+const isValidId = require("../../middlewares/validateId");
 
-router.get("/", async (req, res) => {
-  const result = await contacts.listContacts();
-  res.status(200).json(result);
-});
+router.get("/", ctrlContact.listContacts);
 
-router.get("/:contactId", async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
-  if (!result) {
-    res.status(404).json({ message: "Not found contact their Id" });
-  }
-  res.status(200).json(result);
-});
+router.get("/:contactId", isValidId, ctrlContact.getContactById);
 
-router.post("/", validateBody(schemas.addSchema), async (req, res) => {
-  const { name, email, phone } = req.body;
-  const id = nanoid();
-  const newContact = await contacts.addContact({ id, name, email, phone });
-  res.status(201).json(newContact);
-});
+router.post("/", validateBody(schemas.addSchema), ctrlContact.addContact);
 
-router.delete("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const deletedContacts = await contacts.removeContact(contactId);
-  if (!deletedContacts) {
-    res.status(404).json({ message: "Not found contact with their Id" });
-  }
-  res.status(200).json({ message: "contact deleted" });
-});
+router.delete("/:contactId", isValidId, ctrlContact.removeContact);
 
 router.put(
   "/:contactId",
+  isValidId,
   validateBody(schemas.updateSchema),
-  async (req, res, next) => {
-    const body = req.body;
-    if (!body) {
-      res.status(404).json({ message: "Enter data, please" });
-    }
-    const { contactId } = req.params;
-    const updatedContact = await contacts.updateContact(contactId, body);
-    if (!updatedContact) {
-      res.status(404).json({ message: "Not found contact with their Id" });
-    }
-    res.json(updatedContact);
-  }
+  ctrlContact.updateContact
 );
 
 module.exports = router;
